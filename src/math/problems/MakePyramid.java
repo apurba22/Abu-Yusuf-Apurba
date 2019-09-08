aaae72fdf18cf49e836ab13e5034ff43a331ae36
apurba22 <apurba.dfb22@gmail.com>
2019-09-08 13:14:13 -0400

    matrh.problem MakePyramid

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -1,19 +1,33 @@
 package math.problems;
 
 public class MakePyramid {
-    public static void main(String[] args) {
+    //public static void main(String[] args) {
 
         /*   Implement a large Pyramid of stars in the screen with java.
 
-                              *
-                             * *
-                            * * *
-                           * * * *
-                          * * * * *
-                         * * * * * *
+         *
+         * *
+         * * *
+         * * * *
+         * * * * *
+         * * * * * *
 
-        */
-
-
+         */
+        public static void printPyramid ( int n){
+            for (int i = 0; i < n; i++) {
+                for (int j = n - i; j > 1; j--) {
+                    System.out.print(" ");
+                }
+                for (int j = 0; j <= i; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
         }
-  }
+        public static void main (String args[])
+        {
+            int n = 6;
+            printPyramid(n);
+        }
+
+    }
